0f08bb4ea9c79be88bfa5bdb8724d56601b3b1e2
d.horovenkov <d.horovenkov@ovkgrp.ru>
2023-06-08 21:50:05 +0300

    fix deploy gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './assets/index.css';
 import Router from './components/router/Router';
 import Notification from './components/ui/notification/Notification';
@@ -10,11 +10,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <AuthProvider>
       <Notification>
         <Router />
       </Notification>
     </AuthProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
